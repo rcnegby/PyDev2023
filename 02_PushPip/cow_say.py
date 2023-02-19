4ea67047ebf71d08bf0e228f90faa0fb3e4fd433
rcnegby <rcnegby@mail.ru>
2023-02-19 23:22:02 +0300

    add tongue param

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,12 +3,15 @@ from cowsay import cowsay, list_cows
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', type=str)
+parser.add_argument('-T', type=str)
 parser.add_argument('message', type=str)
 args = parser.parse_args()
 
 params = {}
 if args.e is not None:
     params['eyes'] = args.e[:2]
+if args.e is not None:
+    params['tongue'] = args.T[:2]
 
 params['message'] = args.message
 print(cowsay(**params))
